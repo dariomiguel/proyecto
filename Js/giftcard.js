6bbbf040efdd80cdfdccbf141c3aa81b135131de
Tahiel-Recchia <tahielrecchia05@gmail.com>
2024-10-03 10:36:11 -0300

    Le agregué JS a los botones de giftcard

---
+++ Js/giftcard.js
@@ -0,0 +1,24 @@
+function cambiarColorDeLaGiftCard(){
+    const radios = document.querySelectorAll('input[name="fondos"]');
+    const giftcard = document.getElementById('giftcard');
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', (event) => {
+            giftcard.style.backgroundColor = event.target.value;
+        });
+    });
+}
+
+function cambiarColorDelTextoDeLaGiftCard(){
+    const radios = document.querySelectorAll('input[name="colores"]');
+    const giftcardTexto = document.getElementById('giftcardTexto');
+
+    radios.forEach(radio => {
+        radio.addEventListener('change', (event) => {
+            giftcardTexto.style.color = event.target.value;
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', cambiarColorDelTextoDeLaGiftCard);
+document.addEventListener('DOMContentLoaded', cambiarColorDeLaGiftCard);
