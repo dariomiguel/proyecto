b33a9efa5eb699b6845e2595f55330f0dc5a6d12
Tahiel-Recchia <tahielrecchia05@gmail.com>
2024-10-04 20:11:55 -0300

    Giftcard ya es completamente funcional

--- Js/giftcard.js
+++ Js/giftcard.js
@@ -34,6 +34,66 @@ function cambiarTamañoDelTextoDeLaGiftCard(){
     });
 }
 
+function cambiarElNombreDelDestinatarioEnTiempoReal(){
+    const inputDestinatario = document.getElementById('input__destinatario--Id');
+    const destinatario = document.getElementById('destinatario');
+
+    inputDestinatario.addEventListener('input', function() {
+        destinatario.textContent = inputDestinatario.value || 'destinatario';
+    });
+}
+
+function cambiarElPrecioDeLaGiftcardEnTiempoReal(){
+    const inputMonto = document.getElementById('inputMonto');
+    const monto = document.getElementById('monto');
+
+
+    inputMonto.addEventListener('input', (inputDelUsuario) => {
+        let valorDeLaGiftcard = inputDelUsuario.target.value.replace(/[^0-9]/g, '').slice(0, 6);
+        if (valorDeLaGiftcard.length > 0 && valorDeLaGiftcard[0] < '1') {
+            valorDeLaGiftcard = '';
+        }
+        inputMonto.value = valorDeLaGiftcard;
+        monto.textContent = valorDeLaGiftcard ? `$${valorDeLaGiftcard}.-` : '$0000.-';
+    });
+}
+
+    function cambiarLaPosicionDelMonto() {
+        const divMonto = document.getElementById('giftcardPrecio');
+        const radios = document.querySelectorAll('input[name="ubicacion"]');
+    
+        radios.forEach(radio => {    
+            radio.addEventListener('change', (event) => {
+                const value = event.target.value;
+                // Restablecer las posiciones
+                divMonto.style.top = 'inherit';
+                divMonto.style.right = 'inherit';
+                divMonto.style.bottom = 'inherit';
+                divMonto.style.left = 'inherit';
+                
+                switch (value) {
+                    case 'abajo-derecha':
+                        divMonto.style.bottom = '0';
+                        divMonto.style.right = '0';
+                        break;
+                    case 'arriba-izquierda':
+                        divMonto.style.top = '0';
+                        divMonto.style.left = '0';
+                        break;
+                    case 'arriba-derecha':
+                        divMonto.style.top = '0';
+                        divMonto.style.right = '0';
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+    }
+
+cambiarLaPosicionDelMonto();
+cambiarElPrecioDeLaGiftcardEnTiempoReal();
+cambiarElNombreDelDestinatarioEnTiempoReal();
 cambiarTamañoDelTextoDeLaGiftCard();
 cambiarColorDelTextoDeLaGiftCard();
 cambiarColorDeLaGiftCard();
