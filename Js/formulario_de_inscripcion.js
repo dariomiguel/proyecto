c46c40fb6e3dadfe813ba2aa76addedb37713e02
Darío Miguel <dario.e.miguel@gmail.com>
2024-10-22 10:51:02 -0300

    Se coloca función en botón agregar persona en formulario de inscripcion

--- Js/formulario_de_inscripcion.js
+++ Js/formulario_de_inscripcion.js
@@ -1,1 +1,24 @@
 const botonAgregarPersona = document.getElementById("boton__agregarPersona");
+
+botonAgregarPersona.addEventListener("click", agregarTexto);
+
+function agregarTexto() {
+    let contenedor = document.getElementById("contenedorPersonasAgregadas");
+    let parrafo = document.createElement("div");
+    parrafo.innerHTML = `
+        <div class="inscripcion__input">
+            <label for="curso">Nombre</label>
+            <input id="curso" name="curso" type="text" placeholder="Nombre" required/>
+        </div>
+        <div class="inscripcion__input">
+            <label for="descripcion">Apellido</label>
+            <input id="descripcion" name="descripcion" placeholder="Apellido" required/>
+        </div>
+        <div class="inscripcion__input">
+            <label for="duracion">DNI</label>
+            <input id="tel" name="duracion" placeholder="DNI" pattern="\\d*" title="Por favor, ingrese un DNI válido" required/>
+        </div>
+    `;
+
+    contenedor.appendChild(parrafo);
+}
