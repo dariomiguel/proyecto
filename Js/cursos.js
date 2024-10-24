fd1708eab264c754ef2512fc248a23e56f024c51
Tahiel-Recchia <tahielrecchia05@gmail.com>
2024-10-24 18:44:02 -0300

    Ya estan los cursos agregados dinamicamente

--- Js/cursos.js
+++ Js/cursos.js
@@ -1,3 +1,122 @@
+
+
+const datosDeLosCursos = {
+    cursos: [
+        {
+            id: 1,
+            nombre: "Iniciando en JavaScript",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/javaScript-logo.png",
+            detalle: "/pages/cursos/javscript.html",
+            dataname: "item-programacion",
+        },
+        {
+            id: 2,
+            nombre: "Iniciando en C#",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/cSharp-logo.png",
+            detalle: "/pages/cursos/cSharp.html",
+            dataname: "item-programacion",
+        },
+        {
+            id: 3,
+            nombre: "Iniciando en Illustrator",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/illustrator-logo.png",
+            detalle: "/pages/cursos/illustrator.html",
+            dataname: "item-disenio",
+        },
+        {
+            id: 4,
+            nombre: "Iniciando en MySQL",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/mysql-logo.png",
+            detalle: "/pages/cursos/mysql.html",
+            dataname: "item-data",
+        },
+        {
+            id: 5,
+            nombre: "Iniciando en SQL",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/sql-logo.png",
+            detalle: "/pages/cursos/sql.html",
+            dataname: "item-data",
+        },
+        {
+            id: 6,
+            nombre: "Iniciando en Photoshop",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/photoshop-logo.png",
+            detalle: "/pages/cursos/photoshop.html",
+            dataname: "item-disenio",
+        },
+        {
+            id: 7,
+            nombre: "Iniciando en Python",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/Python_logo_icon.png",
+            detalle: "/pages/cursos/python.html",
+            dataname: "item-programacion",
+        },
+        {
+            id: 8,
+            nombre: "Iniciando en VueJS",
+            duracion: "60hs",
+            precio: "1000",
+            img: "../../img/cardImages/vueJs-logo.png",
+            detalle: "/pages/cursos/vueJs.html",
+            dataname: "item-programacion",
+        }
+    ]
+};
+
+
+localStorage.setItem("cursos", JSON.stringify(datosDeLosCursos.cursos));
+console.log(JSON.parse(localStorage.getItem("cursos")));
+
+
+function mostrarCursos(){
+    const datosDeLosCursos = JSON.parse(localStorage.getItem("cursos"));
+    const cursosContainer = document.getElementById('slider');
+console.log(cursosContainer);
+datosDeLosCursos.forEach((item) => {
+    console.log(item);
+});
+    cursosContainer.innerHTML = '';
+    datosDeLosCursos.forEach((item, index) => {
+        const ContenedorDeCurso = document.createElement('div');
+        ContenedorDeCurso.classList.add("cursos-info-container");
+        ContenedorDeCurso.dataset.name = item.dataname;
+        ContenedorDeCurso.innerHTML =  `<div class="contenedor-imagen__curso">
+            <img src="${item.img}" alt="${item.nombre}" />
+        </div>
+        <div class="cursos-info__detalles">
+            <p class="cursos-info__horas">${item.duracion}</p>
+            <div class="cursos-info__contenedor__titulo-detalles">
+                <p class="cursos-info__titulo">
+                    ${item.nombre}
+                </p>
+                <a href="${item.detalle}">Ver Detalles</a>
+            </div>
+        </div>
+        <div class="precio-cursos">
+        <a class="Compracurso" href="/pages/formulario_de_inscripcion.html"><button class="boton-inscripcion">Comprar</button></a>
+        <p class="precio">$<span id="precioJavascript">${item.precio}</span></p>
+        </div>
+    </div>`
+
+    cursosContainer.appendChild(ContenedorDeCurso);
+    });
+}
+mostrarCursos();
+
 const botonesFiltro = document.querySelectorAll(".contenedor-filtros button");
 const itemsFiltrables = document.querySelectorAll(".slider .cursos-info-container");
 
@@ -16,3 +135,4 @@ const filtrarItems = (e) => {
 }
 
 botonesFiltro.forEach(button => button.addEventListener("click", filtrarItems));
+
