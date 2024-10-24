24b68b428fece70d84061e83593d49f29c3d6573
Dario_DELL <dariomiguel0@gmail.com>
2024-10-23 23:09:30 -0300

    Se cambio la forma de implementar la página de formulario_de_inscripcion.html, ahora se agregan las personas y se deberán seleccionar a partir de los checkbox

--- Js/formulario_de_inscripcion.js
+++ Js/formulario_de_inscripcion.js
@@ -1,32 +1,45 @@
 const botonAgregarPersona = document.getElementById("boton__agregarPersona");
-
 botonAgregarPersona.addEventListener("click", agregarTexto);
+
+let inputNombre = document.getElementById("nombre");
+let inputApellido = document.getElementById("apellido");
+let inputDni = document.getElementById("dni");
 
 function agregarTexto() {
     let contenedor = document.getElementById("contenedorPersonasAgregadas__Id");
     let parrafo = document.createElement("div");
-    parrafo.innerHTML = `
-                <div class="inscripcion">
-                    <div class="inscripcion__contenedor">
-                        
-                        <div class="inscripcion__input">
-                            <label for="curso">Nombre</label>
-                            <input  name="curso" type="text" placeholder="Nombre" required/>
-                        </div>
-                        <div class="inscripcion__input">
-                            <label for="descripcion">Apellido</label>
-                            <input  name="descripcion" placeholder="Apellido" required/>
-                        </div>
+    parrafo.classList.add("formularioDeInscricion__info-container");
 
-                        <div class="inscripcion__input">
-                            <label for="duracion">DNI</label>
-                            <input  name="duracion" placeholder="DNI" pattern="\d*" title="Por favor, ingrese un DNI válido" required/>
-                        </div>
+    let valorDeNombre = inputNombre.value;
+    let valorDeApellido = inputApellido.value;
+    let valorDeDni = inputDni.value;
 
-                        <!-- <button type="button" class="removerCurso"></button> -->
-                    </div>
-                </div>
-    `;
+    // Validar si algún campo está vacío
+    if (
+        !(valorDeNombre === "" || valorDeApellido === "" || valorDeDni === "")
+    ) {
+        parrafo.innerHTML = `
+            <p>${valorDeApellido}, ${valorDeNombre} - DNI:${valorDeDni}</p>
+            <input type="checkbox" name="alumnoAInscribir" value="${valorDeDni}" id="${valorDeDni}">
 
-    contenedor.appendChild(parrafo);
+        `;
+
+        contenedor.appendChild(parrafo);
+
+        inputNombre.value = "";
+        inputApellido.value = "";
+        inputDni.value = "";
+    }
 }
+
+const botonConfirmarInscriptos = document.getElementById("boton__confirmar");
+botonConfirmarInscriptos.addEventListener("click", function () {
+    const checkboxes = document.querySelectorAll(
+        'input[name="alumnoAInscribir"]:checked'
+    );
+    const valoresSeleccionados = [];
+
+    checkboxes.forEach((checkbox) => {
+        valoresSeleccionados.push(checkbox.value);
+    });
+});
