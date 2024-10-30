3d834a5152d458d290bba4d511363a3bf21bea66
Dario_DELL <dariomiguel0@gmail.com>
2024-10-30 18:25:12 -0300

    Intento de reparación de cursos.js

--- Js/cursos.js
+++ Js/cursos.js
@@ -1,5 +1,3 @@
-
-
 const datosDeLosCursos = {
     cursos: [
         {
@@ -8,7 +6,8 @@ const datosDeLosCursos = {
             duracion: "60hs",
             precio: "1500",
             img: "../../img/cardImages/javaScript-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-programacion",
         },
@@ -18,7 +17,8 @@ const datosDeLosCursos = {
             duracion: "120hs",
             precio: "3000",
             img: "../../img/cardImages/cSharp-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-programacion",
         },
@@ -28,7 +28,8 @@ const datosDeLosCursos = {
             duracion: "40hs",
             precio: "1200",
             img: "../../img/cardImages/illustrator-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-disenio",
         },
@@ -38,7 +39,8 @@ const datosDeLosCursos = {
             duracion: "80hs",
             precio: "2300",
             img: "../../img/cardImages/mysql-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-data",
         },
@@ -48,7 +50,8 @@ const datosDeLosCursos = {
             duracion: "80hs",
             precio: "2000",
             img: "../../img/cardImages/sql-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-data",
         },
@@ -58,7 +61,8 @@ const datosDeLosCursos = {
             duracion: "50hs",
             precio: "1500",
             img: "../../img/cardImages/photoshop-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-disenio",
         },
@@ -68,7 +72,8 @@ const datosDeLosCursos = {
             duracion: "60hs",
             precio: "1800",
             img: "../../img/cardImages/Python_logo_icon.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-programacion",
         },
@@ -78,7 +83,8 @@ const datosDeLosCursos = {
             duracion: "70hs",
             precio: "1600",
             img: "../../img/cardImages/vueJs-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-programacion",
         },
@@ -88,9 +94,10 @@ const datosDeLosCursos = {
             duracion: "60hs",
             precio: "1900",
             img: "../../img/cardImages/Scala-logo.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
-            dataname: "item-data"
+            dataname: "item-data",
         },
         {
             id: 10,
@@ -98,26 +105,25 @@ const datosDeLosCursos = {
             duracion: "50hs",
             precio: "1300",
             img: "../../img/cardImages/adobe-express.png",
-            descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
+            descripcion:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate vitae, dolor provident minima consequuntur a inventore necessitatibus temporibus voluptatem",
             requisitos: "Ninguno",
             dataname: "item-disenio",
-        }
-    ]
+        },
+    ],
 };
-
 
 localStorage.setItem("cursos", JSON.stringify(datosDeLosCursos.cursos));
 
-
-function mostrarCursos(){
+function mostrarCursos() {
     const datosDeLosCursos = JSON.parse(localStorage.getItem("cursos"));
-    const cursosContainer = document.getElementById('slider');
-    cursosContainer.innerHTML = '';
+    const cursosContainer = document.getElementById("slider");
+    cursosContainer.innerHTML = "";
     datosDeLosCursos.forEach((item) => {
-        const ContenedorDeCurso = document.createElement('div');
+        const ContenedorDeCurso = document.createElement("div");
         ContenedorDeCurso.classList.add("cursos-info-container");
         ContenedorDeCurso.dataset.name = item.dataname;
-        ContenedorDeCurso.innerHTML =  `<div class="contenedor-imagen__curso">
+        ContenedorDeCurso.innerHTML = `<div class="contenedor-imagen__curso">
             <img src="${item.img}" alt="${item.nombre}" />
         </div>
         <div class="cursos-info__detalles">
@@ -133,9 +139,9 @@ function mostrarCursos(){
         <button class="boton-inscripcion" id="JS-botonCompra" onclick="agregarAlCarrito(${item.id})">Comprar</button>
         <p class="precio">$<span id="precioJavascript">${item.precio}</span></p>
         </div>
-    </div>`
+    </div>`;
 
-    cursosContainer.appendChild(ContenedorDeCurso);
+        cursosContainer.appendChild(ContenedorDeCurso);
     });
 }
 mostrarCursos();
@@ -143,25 +149,25 @@ mostrarCursos();
 const botonesFiltro = document.querySelectorAll(".contenedor-filtros button");
 const itemsFiltrables = document.querySelectorAll(".slider .cursos-info-container");
 
-
-
-const botonCompra = document.getElementById('JS-botonCompra');
-let carritoDeCompras = JSON.parse(localStorage.getItem('carrito')) || [];
+const botonCompra = document.getElementById("JS-botonCompra");
+let carritoDeCompras = JSON.parse(localStorage.getItem("carrito")) || [];
 function agregarAlCarrito(id) {
-    const agregadoAlCarrito = document.getElementById('JS-agregadoAlCarrito');
+    const agregadoAlCarrito = document.getElementById("JS-agregadoAlCarrito");
     console.log(carritoDeCompras);
-    const curso = curso.find(c => c.id === id);
+    let cursos = JSON.parse(localStorage.getItem("cursos")); // Obtener los cursos del localStorage
+
+    const curso = cursos.find((c) => c.id === id);
     if (curso) {
-        const existe = carritoDeCompras.find(item => item.id === curso.id);
-        if(!existe){
-        carritoDeCompras.push(curso);
+        const existe = carritoDeCompras.find((item) => item.id === curso.id);
+        if (!existe) {
+            carritoDeCompras.push(curso);
         }
-        agregadoAlCarrito.classList.add('visible');
-        setTimeout(() =>{
-            agregadoAlCarrito.classList.remove('visible');
+        agregadoAlCarrito.classList.add("visible");
+        setTimeout(() => {
+            agregadoAlCarrito.classList.remove("visible");
         }, 2000);
-        localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
-        console.log(JSON.parse(localStorage.getItem('carrito')));
+        localStorage.setItem("carrito", JSON.stringify(carritoDeCompras));
+        console.log(JSON.parse(localStorage.getItem("carrito")));
     }
 }
 
@@ -169,20 +175,23 @@ const filtrarItems = (e) => {
     document.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
 
-    itemsFiltrables.forEach(item =>{
+    itemsFiltrables.forEach((item) => {
         item.classList.add("hide");
-        if(item.dataset.name === e.target.dataset.name || e.target.dataset.name === "all-courses"){
+        if (
+            item.dataset.name === e.target.dataset.name ||
+            e.target.dataset.name === "all-courses"
+        ) {
             item.classList.remove("hide");
         }
-    })
-}
+    });
+};
 
-botonesFiltro.forEach(button => button.addEventListener("click", filtrarItems));
+botonesFiltro.forEach((button) => button.addEventListener("click", filtrarItems));
 
-function mostrarDetalles(id){
+function mostrarDetalles(id) {
     const cursos = JSON.parse(localStorage.getItem("cursos"));
-    const cursoSeleccionado = cursos.find(curso => curso.id === id);
-    cursoSeleccionado.innerHTML = '';
+    const cursoSeleccionado = cursos.find((curso) => curso.id === id);
+    cursoSeleccionado.innerHTML = "";
 
     document.getElementById("js-imagen-curso").scr = cursoSeleccionado.img;
     document.getElementById("js-titulo-curso").innerHTML = cursoSeleccionado.nombre;
