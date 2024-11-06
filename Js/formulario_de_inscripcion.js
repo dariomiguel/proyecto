25bd5dd67f8a726d2ea4a9cfbfd6571b16a7208b
Dario_DELL <dariomiguel0@gmail.com>
2024-11-06 04:00:35 -0300

    Intento por agregar las sumas de los checkbox

--- Js/formulario_de_inscripcion.js
+++ Js/formulario_de_inscripcion.js
@@ -1,12 +1,10 @@
-const cursosDisponibles = ["javascript","c","illustrator","Mysql","SQL","Photoshop","Python","Vuejs","Scala","Express"];
-
 const botonAgregarPersona = document.getElementById("boton__agregarPersona");
 botonAgregarPersona.addEventListener("click", agregarTexto);
 
 let inputNombre = document.getElementById("nombre");
 let inputApellido = document.getElementById("apellido");
 let inputDni = document.getElementById("dni");
-let inputCurso = document.getElementById("curso");
+let valorCurso1 = document.getElementById("importe");
 
 function agregarTexto() {
     let contenedor = document.getElementById("contenedorPersonasAgregadas__Id");
@@ -16,20 +14,15 @@ function agregarTexto() {
     let valorDeNombre = inputNombre.value;
     let valorDeApellido = inputApellido.value;
     let valorDeDni = inputDni.value;
-    let valorCurso = inputCurso.value;
-
-
-    if (!validarCurso(valorCurso)) {
-        alert("curso invalido");
-        return;
-    }
+    let valorCurso = valorCurso1.value;
 
     // Validar si algún campo está vacío
-    if (!(valorDeNombre === "" || valorDeApellido === "" || valorDeDni === "" || valorCurso === "")) {
-        
+    if (
+        !(valorDeNombre === "" || valorDeApellido === "" || valorDeDni === "" || valorCurso === "")
+    ) {
         parrafo.innerHTML = `
             <label for="${valorDeDni}">${valorDeApellido}, ${valorDeNombre} - DNI:${valorDeDni} , Curso:${valorCurso}</label>
-            <input type="checkbox" name="alumnoAInscribir" value="${valorDeDni}" id="${valorDeDni}" data-curso="${valorCurso}" required>
+            <input type="checkbox" class="terra" name="${valorDeDni}" value="20" id="${valorDeDni}" data-curso="${valorCurso}" required>
             <button type="button" class="eliminarInscripto" onclick="eliminarInscripto('${valorDeDni}')">Eliminar</button>
         `;
 
@@ -38,54 +31,68 @@ function agregarTexto() {
         inputNombre.value = "";
         inputApellido.value = "";
         inputDni.value = "";
-        inputCurso.value="";
     }
 }
 
-function eliminarInscripto(dniAux){
-    let inscripto = document.querySelectorAll(".formularioDeInscricion__info-container")
-    inscripto.forEach(inscripcion =>{
+function eliminarInscripto(dniAux) {
+    let inscripto = document.querySelectorAll(".formularioDeInscricion__info-container");
+    inscripto.forEach((inscripcion) => {
         let dniInput = inscripcion.querySelector("input[type='checkbox']");
-        if(dniInput && dniInput.value == dniAux){
+        if (dniInput && dniInput.name == dniAux) {
             inscripcion.remove();
         }
-    })
+    });
 }
 
+const checkboxes = document.querySelectorAll(".terra");
 
-const botonConfirmarInscriptos = document.getElementById("boton__confirmar");
-let usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
-let total = 0;
-
-
-botonConfirmarInscriptos.addEventListener("click", function () {
-
-    const checkboxes = document.querySelectorAll('input[name="alumnoAInscribir"]:checked');
-    const valoresSeleccionados = JSON.parse(localStorage.getItem(`personasInscriptasPor${usuarioLogueado.nombre}`)) || [];
-
+// Función para actualizar el total
+function updateTotal() {
+    let total = 10;
     checkboxes.forEach((checkbox) => {
-        let persona = {
-            dni:checkbox.value,
-            curso:checkbox.getAttribute("data-curso"),
-        };
-        valoresSeleccionados.push(persona);
+        if (checkbox.checked) {
+            total += parseInt(checkbox.value);
+        }
     });
-    total+=20 * valoresSeleccionados.length;
-    localStorage.setItem("totalCursos",JSON.stringify(total));
-    localStorage.setItem(`personasInscriptasPor${usuarioLogueado.nombre}`,JSON.stringify(valoresSeleccionados));
-    
+    document.getElementById("valorTotal").textContent = total;
+}
+
+// Agregamos un evento de cambio a cada checkbox
+checkboxes.forEach((checkbox) => {
+    console.log("hola");
+    checkbox.addEventListener("change", updateTotal());
 });
 
+// const botonConfirmarInscriptos = document.getElementById("boton__confirmar");
+// let usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
 
+// botonConfirmarInscriptos.addEventListener("click", function () {
+//     const checkboxes = document.querySelectorAll('input[name="alumnoAInscribir"]:checked');
+//     const valoresSeleccionados =
+//         JSON.parse(localStorage.getItem(`personasInscriptasPor${usuarioLogueado.nombre}`)) || [];
 
+//     checkboxes.forEach((checkbox) => {
+//         let persona = {
+//             dni: checkbox.value,
+//             curso: checkbox.getAttribute("data-curso"),
+//         };
+//         valoresSeleccionados.push(persona);
+//     });
+//     total += 20 * valoresSeleccionados.length;
+//     localStorage.setItem("totalCursos", JSON.stringify(total));
+//     localStorage.setItem(
+//         `personasInscriptasPor${usuarioLogueado.nombre}`,
+//         JSON.stringify(valoresSeleccionados)
+//     );
+// });
 
-function validarCurso(curso){
-    let encontrado = false;
-    cursosDisponibles.forEach(aux=>{
-        if(aux.toLowerCase() == curso.toLowerCase()){
-            encontrado = true;
-        }
-    })
+// function validarCurso(curso) {
+//     let encontrado = false;
+//     cursosDisponibles.forEach((aux) => {
+//         if (aux.toLowerCase() == curso.toLowerCase()) {
+//             encontrado = true;
+//         }
+//     });
 
-    return encontrado;
-}
+//     return encontrado;
+// }
