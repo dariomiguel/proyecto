a8287fb5cffed51c492bfeea1d4dbedebca0fd0e
Francisco Larralde <larraldefrancisco19@gmail.com>
2024-11-06 09:49:45 -0300

    detalles dinamicos en calendario

--- Js/popupCalendario.js
+++ Js/popupCalendario.js
@@ -3,61 +3,61 @@ const cursos = [
         nombre: "JavaScript",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:1,img: "../../img/cardImages/javaScript-logo.png",duracion:"60hs",precio:"1500"
+        link: "#js-descripcion__contenido",id:1,img: "../../img/cardImages/javaScript-logo.png",duracion:"60hs",precio:"1500"
     },
     {
         nombre: "C",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:2,img: "../../img/cardImages/cSharp-logo.png",duracion:"120hs",precio:"3000"
+        link: "#js-descripcion__contenido",id:2,img: "../../img/cardImages/cSharp-logo.png",duracion:"120hs",precio:"3000"
     },
     {
         nombre: "Illustrator",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:3,img: "../../img/cardImages/illustrator-logo.png",duracion:"40hs",precio:"1200"
+        link: "#js-descripcion__contenido",id:3,img: "../../img/cardImages/illustrator-logo.png",duracion:"40hs",precio:"1200"
     },
     {
         nombre: "Mysql",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:4,img: "../../img/cardImages/mysql-logo.png",duracion:"80hs",precio:"2300"
+        link: "#js-descripcion__contenido",id:4,img: "../../img/cardImages/mysql-logo.png",duracion:"80hs",precio:"2300"
     },
     {
         nombre: "SQL",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:5,img: "../../img/cardImages/sql-logo.png",duracion:"80hs",precio:"2000"
+        link: "#js-descripcion__contenido",id:5,img: "../../img/cardImages/sql-logo.png",duracion:"80hs",precio:"2000"
     },
     {
         nombre: "Photoshop",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:6,img: "../../img/cardImages/photoshop-logo.png",duracion:"50hs",precio:"1500"
+        link: "#js-descripcion__contenido",id:6,img: "../../img/cardImages/photoshop-logo.png",duracion:"50hs",precio:"1500"
     },
     {
         nombre: "Python",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:7,img: "../../img/cardImages/Python_logo_icon.png",duracion:"60hs",precio:"1800"
+        link: "#js-descripcion__contenido",id:7,img: "../../img/cardImages/Python_logo_icon.png",duracion:"60hs",precio:"1800"
     },
     {
         nombre: "Vuejs",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:8,img: "../../img/cardImages/vueJs-logo.png",duracion:"70hs",precio:"1600"
+        link: "#js-descripcion__contenido",id:8,img: "../../img/cardImages/vueJs-logo.png",duracion:"70hs",precio:"1600"
     },
     {
         nombre: "Scala",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:9,img: "../../img/cardImages/Scala-logo.png",duracion:"60hs",precio:"1900"
+        link: "#js-descripcion__contenido",id:9,img: "../../img/cardImages/Scala-logo.png",duracion:"60hs",precio:"1900"
     },
     {
         nombre: "Express",
         resumen:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam voluptatem quam voluptate. Iure eos enim nisi dolores velit facilis veniam quia, repellat aperiam voluptates qui! Illum placeat quibusdam similique excepturi?",
-        link: "#",id:10,img: "../../img/cardImages/adobe-express.png",duracion:"50hs",precio:"1300"
+        link: "#js-descripcion__contenido",id:10,img: "../../img/cardImages/adobe-express.png",duracion:"50hs",precio:"1300"
     },
 ];
 
@@ -78,7 +78,7 @@ function mostrarPopup(aux) {
             popup.innerHTML = `
             <h2>Nuevo curso de ${curso.nombre}</h2>
             <p>${curso.resumen}<p>
-            <a href=${curso.link}>Ver Detalle</a>
+            <a href=${curso.link} onclick="mostrarDetalles(${curso.id})">Ver Detalle</a>
             <button class="agregarAlCarrito ocultoEnSesionCerrada" onclick="agregarAlCarrito(${curso.id})">Agregar al carrito</button>
             <button class="botonCerrar">Cerrar</button>`;
 
@@ -127,5 +127,49 @@ function agregarAlCarrito(id) {
         }, 2000);
         localStorage.setItem(`carrito_${usuarioEnSesion.correo}`, JSON.stringify(carritoDeCompras));
         console.log(JSON.parse(localStorage.getItem(`carrito_${usuarioEnSesion.correo}`)));
+    }
+}
+
+function mostrarDetalles(id) {
+    const cursos = JSON.parse(localStorage.getItem("cursos"));
+    const cursoSeleccionado = cursos.find((curso) => curso.id === id);
+    cursoSeleccionado.innerHTML = "";
+
+    if (cursoSeleccionado) {
+        // Almacenar cada propiedad individual en sessionStorage
+        sessionStorage.setItem("cursoId", cursoSeleccionado.id);
+        sessionStorage.setItem("cursoNombre", cursoSeleccionado.nombre);
+        sessionStorage.setItem("cursoImagen", cursoSeleccionado.img);
+        sessionStorage.setItem("cursoPrecio", cursoSeleccionado.precio);
+        sessionStorage.setItem("cursoDuracion", cursoSeleccionado.duracion);
+        sessionStorage.setItem("cursoDescripcion", cursoSeleccionado.descripcion);
+        sessionStorage.setItem("cursoRequisitos", cursoSeleccionado.requisitos);
+    }
+
+    const imagenCurso = document.getElementById("js-imagen-curso");
+    const tituloCurso = document.getElementById("js-titulo-curso");
+    const valorCurso = document.getElementById("js-valor-curso");
+    const duracionCurso = document.getElementById("js-duracion-curso");
+    const descripcionCurso = document.getElementById("js-descripcion-curso");
+    const requisitosCurso = document.getElementById("js-requisitos-curso");
+
+    if (
+        imagenCurso != null &&
+        tituloCurso != null &&
+        valorCurso != null &&
+        duracionCurso != null &&
+        descripcionCurso != null &&
+        requisitosCurso != null
+    ) {
+        imagenCurso.src = cursoSeleccionado.img;
+        tituloCurso.innerHTML = cursoSeleccionado.nombre;
+        valorCurso.innerHTML = cursoSeleccionado.precio;
+        duracionCurso.innerHTML = cursoSeleccionado.duracion;
+        descripcionCurso.innerHTML = cursoSeleccionado.descripcion;
+        requisitosCurso.innerHTML = cursoSeleccionado.requisitos;
+    } else if (localStorage.getItem("estadoDeSesion") == null) {
+        window.location.href = "../pages/login.html";
+    } else {
+        window.location.href = "../pages/cursos.html";
     }
 }
